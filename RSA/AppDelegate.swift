edf0f728fb50e6885615b9e5959ba44644964681
Thomas JEZEQUEL <jezequel.tho@gmail.com>
2017-06-27 22:15:55 +0200

    Initial Commit

---
+++ RSA/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  RSA
+//
+//  Created by Thomas Jezequel on 27/06/2017.
+//  Copyright © 2017 Thomas Jezequel. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ RSA/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  RSA
+//
+//  Created by Thomas Jezequel on 27/06/2017.
+//  Copyright © 2017 Thomas Jezequel. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
